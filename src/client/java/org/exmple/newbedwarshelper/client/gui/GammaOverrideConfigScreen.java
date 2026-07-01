4f6a79580729dd0ea0942475f1fb2cf05f4ccb62
8BitsOneByte <201945071+8BitsOneByte@users.noreply.github.com>
2026-07-01 14:23:06 +0800

    Gamma Override feature Release

--- src/client/java/org/exmple/newbedwarshelper/client/gui/GammaOverrideConfigScreen.java
+++ src/client/java/org/exmple/newbedwarshelper/client/gui/GammaOverrideConfigScreen.java
@@ -1,13 +1,16 @@
 package org.exmple.newbedwarshelper.client.gui;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.CycleButton;
 import net.minecraft.client.gui.components.StringWidget;
+import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.layouts.FrameLayout;
 import net.minecraft.client.gui.layouts.GridLayout;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import org.exmple.newbedwarshelper.client.gammaoverride.GammaOverrideIrisCompat;
 import org.exmple.newbedwarshelper.client.gammaoverride.GammaOverrideManager;
 import org.exmple.newbedwarshelper.client.z_config.ModConfig;
 
@@ -17,6 +20,7 @@ public class GammaOverrideConfigScreen extends Screen {
     private static final Component ENABLED_OFF_TEXT = Component.translatable("screen.newbedwarshelper.gamma_override.enabled.off");
     private static final Component MODE_NIGHT_VISION_TEXT = Component.translatable("screen.newbedwarshelper.gamma_override.mode.night_vision");
     private static final Component MODE_INVALID_GAMMA_TEXT = Component.translatable("screen.newbedwarshelper.gamma_override.mode.invalid_gamma");
+    private static final Component SHADER_INCOMPATIBLE_TOOLTIP = Component.translatable("screen.newbedwarshelper.gamma_override.mode.invalid_gamma.shader_incompatible").withStyle(ChatFormatting.RED);
     private static final Component DONE_TEXT = Component.translatable("screen.newbedwarshelper.gamma_override.done");
     private static final int BUTTON_WIDTH = 180;
 
@@ -67,12 +71,25 @@ public class GammaOverrideConfigScreen extends Screen {
         CycleButton<ModConfig.GammaOverrideMode> button = CycleButton.builder(GammaOverrideConfigScreen::modeText, GammaOverrideManager.getMode())
                 .withValues(ModConfig.GammaOverrideMode.NIGHT_VISION, ModConfig.GammaOverrideMode.INVALID_GAMMA)
                 .displayOnlyValue()
-                .create(Component.empty(), (cycleButton, mode) -> GammaOverrideManager.setMode(mode));
+                .create(Component.empty(), (cycleButton, mode) -> {
+                    GammaOverrideManager.setMode(mode);
+                    updateModeButtonTooltip(cycleButton, mode);
+                });
         button.setWidth(BUTTON_WIDTH);
+        updateModeButtonTooltip(button, GammaOverrideManager.getMode());
         return button;
     }
 
     private static Component modeText(ModConfig.GammaOverrideMode mode) {
         return mode == ModConfig.GammaOverrideMode.INVALID_GAMMA ? MODE_INVALID_GAMMA_TEXT : MODE_NIGHT_VISION_TEXT;
+    }
+
+    private static void updateModeButtonTooltip(CycleButton<ModConfig.GammaOverrideMode> button, ModConfig.GammaOverrideMode mode) {
+        if (mode == ModConfig.GammaOverrideMode.INVALID_GAMMA && GammaOverrideIrisCompat.isShaderPackInUse()) {
+            button.setTooltip(Tooltip.create(SHADER_INCOMPATIBLE_TOOLTIP));
+            return;
+        }
+
+        button.setTooltip(null);
     }
 }
